466db6e2b846db8e256bc2e483df87e0b2e8cc13
Giuseppe <giusepperocco38@gmail.com>
2023-02-20 15:14:16 +0100

    Initial Commit

---
+++ MC3app/MC3appApp.swift
@@ -0,0 +1,20 @@
+//
+//  MC3appApp.swift
+//  MC3app
+//
+//  Created by Giuseppe Rocco on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MC3appApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
