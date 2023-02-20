2c7e73f5f59cd2c55bb74d8e90e926080102e8d5
Aryan-Garg-1 <aryangarg71187@gmail.com>
2023-02-20 16:35:33 +0100

    Stop Watch Logic Complete ( Designing and some integrations left )

---
+++ MC3app/StopWatch.swift
@@ -0,0 +1,82 @@
+//
+//  StopWatch.swift
+//  MC3app
+//
+//  Created by Aryan Garg on 20/02/23.
+//
+
+import SwiftUI
+
+struct StopWatch: View {
+    
+    @ObservedObject var stopWatchManager = StopWatchManager()
+    
+    var body: some View {
+        NavigationStack{
+            VStack{
+                Text("Study Time")
+                Text(String(format:"%.1f", stopWatchManager.secondsElapsed))
+                    .font(.custom("Avenir", size: 60))
+                    .frame(width: 200, height: 100, alignment: .leading)
+                //                    .padding(.top,200)
+                //                    .padding(.bottom,100)
+                //                    .padding(.trailing,100)
+                //                    .padding(.leading,100)
+            
+                Text("Break")
+                Text(String(format:"%.1f", stopWatchManager.studyBreak))
+                    .font(.custom("Avenir", size: 60))
+                    .frame(width: 200, height: 100, alignment: .leading)
+                //                    .padding(.top,200)
+                //                    .padding(.bottom,100)
+                //                    .padding(.trailing,100)
+                //                    .padding(.leading,100)
+                if stopWatchManager.mode == .stopped{
+                    Button(action: {self.stopWatchManager.Start()}) {
+                        TimerButton(label: "Start", buttonColor: .yellow, textColor: .black)
+                    }
+                }
+                if stopWatchManager.mode == .running {
+                    Button(action: {self.stopWatchManager.breakOn()}) {
+                        TimerButton(label: "Break On", buttonColor: .yellow, textColor: .black)
+                    }
+                    Button(action: {self.stopWatchManager.stop()}) {
+                        TimerButton(label: "Finish", buttonColor: .red, textColor: .white)
+                    }
+                }
+                
+                if stopWatchManager.mode == .paused {
+                    
+                    Button(action: {self.stopWatchManager.breakOff()}) {
+                        TimerButton(label: "Break off", buttonColor: .yellow, textColor: .black)
+                    }
+                    Button(action: {self.stopWatchManager.stop()}) {
+                        TimerButton(label: "Finish", buttonColor: .red, textColor: .white)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct StopWatch_Previews: PreviewProvider {
+    static var previews: some View {
+        StopWatch()
+    }
+}
+
+struct TimerButton:View{
+    
+    let label: String
+    let buttonColor: Color
+    let textColor: Color
+    
+    var body: some View{
+        Text(label)
+            .foregroundColor(textColor)
+            .padding(.vertical, 20)
+            .padding(.horizontal, 90)
+            .background(buttonColor)
+            .cornerRadius(10)
+    }
+}
--- MC3app/StopWatchManager.swift
+++ MC3app/StopWatchManager.swift
@@ -12,13 +12,18 @@ class StopWatchManager: ObservableObject{
     
     enum stopWatchMode{
         case running
-        case stopped
+        case breakOn
+        case breakOff
         case paused
+        case stopped
     }
     
     @Published var mode : stopWatchMode = .stopped
     
     @Published  var secondsElapsed = 0.0
+    
+    @Published var studyBreak = 0.0
+    
     var timer = Timer()
     
     func Start(){
@@ -28,15 +33,28 @@ class StopWatchManager: ObservableObject{
         }
     }
     
-    func pause(){
+    func breakOn(){
         timer.invalidate()
         mode = .paused
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            self.studyBreak += 0.1
+        }
+    }
+    
+    func breakOff(){
+        timer.invalidate()
+        mode = .paused
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            self.secondsElapsed += 0.1
+        }
     }
     
     func stop(){
         timer.invalidate()
         secondsElapsed = 0
+        studyBreak = 0
         mode = .stopped
     }
+    
 }
 
