e3e7a20042fde2af29a0b1d5712e2d4892751f12
Aryan-Garg-1 <aryangarg71187@gmail.com>
2023-02-22 14:11:01 +0100

    Watch Changes hide and show and detail view page

--- MC3app/StopWatch.swift
+++ MC3app/StopWatch.swift
@@ -14,50 +14,68 @@ struct StopWatch: View {
     var body: some View {
         NavigationStack{
             VStack{
-                HStack{
-                    Text("Study Time")
-                        .font(.system(size: 30))
-                    
-                    if stopWatchManager.mode == .running {
-                        
-                        Button(action: {self.stopWatchManager.breakOn()}) {
-                            TimerButton(label: "Break", buttonColor: .yellow, textColor: .black)
+                VStack{
+                    if stopWatchManager.mode == .running || stopWatchManager.mode == .breakOff || stopWatchManager.mode == .stopped{
+                        HStack{
+                            Text("Study Time")
+                                .font(.system(size: 30))
+                            
+                            
+                            if stopWatchManager.mode == .running || stopWatchManager.mode == .breakOff  {
+                                
+                                Button(action: {self.stopWatchManager.breakOn()}) {
+                                    TimerButton(label: "Break", buttonColor: .yellow, textColor: .black)
+                                }
+                            }
+                            
+                            if stopWatchManager.mode == .stopped || stopWatchManager.mode == .breakOn{
+                                Button(action:{}){
+                                    TimerButton(label: "Break", buttonColor: .gray, textColor: .white)
+                                }
+                            }
+                            
                         }
-                    }
-                    if stopWatchManager.mode == .breakOff {
                         
-                        Button(action: {self.stopWatchManager.breakOn()}) {
-                            TimerButton(label: "Break", buttonColor: .yellow, textColor: .black)
+                        HStack(spacing: 2) {
+                            StopwatchUnitView(timeUnit: stopWatchManager.hours)
+                            Text(":")
+                            StopwatchUnitView(timeUnit: stopWatchManager.minutes)
+                            Text(":")
+                            StopwatchUnitView(timeUnit: stopWatchManager.seconds)
                         }
+                        .font(.custom("Avenir", size:40))
                     }
-                }
-                HStack(spacing: 2) {
-                    StopwatchUnitView(timeUnit: stopWatchManager.hours)
-                    Text(":")
-                    StopwatchUnitView(timeUnit: stopWatchManager.minutes)
-                    Text(":")
-                    StopwatchUnitView(timeUnit: stopWatchManager.seconds)
-                }
-                .font(.custom("Avenir", size:40))
-                HStack{
-                    Text("Break")
-                        .font(.system(size: 30))
-                    if stopWatchManager.mode == .breakOn {
-                        
-                        Button(action: {self.stopWatchManager.breakOff()}) {
-                            TimerButton(label: "Study", buttonColor: .yellow, textColor: .black)
-                        }
-                    }
-                }
-                HStack(spacing: 2) {
-                    StopwatchUnitView(timeUnit: stopWatchManager.hours_2)
-                    Text(":")
-                    StopwatchUnitView(timeUnit: stopWatchManager.minutes_2)
-                    Text(":")
-                    StopwatchUnitView(timeUnit: stopWatchManager.seconds_2)
-                }
-                .font(.custom("Avenir", size: 40))
+                }.position(x:150, y: 150)
                 
+                VStack{
+                    
+                    if stopWatchManager.mode == .breakOn {
+                        HStack{
+                            Text("Break")
+                                .font(.system(size: 30))
+                            if stopWatchManager.mode == .breakOn {
+                                
+                                Button(action: {self.stopWatchManager.breakOff()}) {
+                                    TimerButton(label: "Study", buttonColor: .yellow, textColor: .black)
+                                }
+                            }
+                            //                            if stopWatchManager.mode == .stopped || stopWatchManager.mode == .breakOff ||
+                            //                                stopWatchManager.mode == .running{
+                            //                                Button(action:{}){
+                            //                                    TimerButton(label: "Break", buttonColor: .gray, textColor: .black)
+                            //                                }
+                            //                            }
+                        }
+                        HStack(spacing: 2) {
+                            StopwatchUnitView(timeUnit: stopWatchManager.hours_2)
+                            Text(":")
+                            StopwatchUnitView(timeUnit: stopWatchManager.minutes_2)
+                            Text(":")
+                            StopwatchUnitView(timeUnit: stopWatchManager.seconds_2)
+                        }
+                        .font(.custom("Avenir", size: 40))
+                    }
+                }.position(x:150, y: 20)
                 if stopWatchManager.mode == .stopped{
                     Button(action: {self.stopWatchManager.Start()}) {
                         TimerButton(label: "Start", buttonColor: .yellow, textColor: .black)
@@ -90,11 +108,11 @@ struct StopWatch: View {
                         .sheet(isPresented: $showModal) {
                             SheetView(showModal: $showModal, studyhours: Int16(stopWatchManager.hours), studyminutes: Int16(stopWatchManager.minutes), studyseconds: Int16(stopWatchManager.seconds), breakhours: Int16(stopWatchManager.hours_2), breakminutes: Int16(stopWatchManager.minutes_2), breakseconds: Int16(stopWatchManager.seconds_2))
                         }
-                    }
                 }
             }
         }
     }
+}
 
 struct StopWatch_Previews: PreviewProvider {
     static var previews: some View {
