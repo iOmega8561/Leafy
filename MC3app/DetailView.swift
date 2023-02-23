979dccc97a7df9ac1f449677d0180793c6a0c988
Giuseppe <giusepperocco38@gmail.com>
2023-02-23 16:56:53 +0100

    DetailView finished

--- MC3app/DetailView.swift
+++ MC3app/DetailView.swift
@@ -12,111 +12,136 @@ struct DetailView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
     var item: FetchedResults<Item>.Element
+
+    init(item: FetchedResults<Item>.Element) {
+        self.item = item
+        
+        //Use this if NavigationBarTitle is with Large Font
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color("TextColor"))]
+        
+        //Use this if NavigationBarTitle is with displayMode = .inline
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color("TextColor"))]
+    }
     
     var body: some View {
         GeometryReader { proxy in
             NavigationStack {
-                ScrollView {
-                    VStack(spacing: 20.0) {
-                        HStack {
-                            Spacer().frame(maxWidth: 30.0)
-                            Text(item.subject ?? "No subject!").font(.system(size: 34.0, weight: .bold))
-                            Spacer()
-                        }
-                        
-                        
-                        
-                        HStack {
-                            Spacer().frame(maxWidth: 30.0)
-                            
-                            Text(item.whatdidyoustudy ?? "Nothing here!")
-                                .font(.system(size: 20.0))
-                                .multilineTextAlignment(.leading)
-                            
-                            Spacer()
-                        }
-                        
-                        RoundedRectangle(cornerRadius: 10.0)
-                            .frame(maxWidth: proxy.size.width * 0.85, minHeight: 100)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 7.0)
-                                    .frame(maxWidth: proxy.size.width * 0.83, maxHeight: 94)
-                                    .foregroundColor(.white)
-                                    .overlay(
-                                        HStack {
-                                            Spacer().frame(maxWidth: 20.0)
-                                            
-                                            Image(imgs[Int(item.emotion)])
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(maxWidth: 70.0)
-                                            
-                                            Spacer().frame(maxWidth: 15.0)
-                                            
-                                            VStack(spacing: 7.0) {
-                                                Text("Study time:")
-                                                    .font(.system(size: 20.0))
-                                                
-                                                Text("Break time:")
-                                                    .font(.system(size: 20.0))
-                                            }
-                                            
-                                            Spacer().frame(maxWidth: 10.0)
-                                            
-                                            VStack(spacing: 7.0) {
-                                                Text("\(item.studyhours)h \(item.studyminutes)m")
-                                                    .font(.system(size: 20.0))
-                                                
-                                                Text("\(item.breakhours)h \(item.breakminutes)m")
-                                                    .font(.system(size: 20.0))
-                                            }
-                                            
-                                            Spacer().frame(maxWidth: 40.0)
-                                        }
-                                    )
-                            )
-                        
-                        
-                        HStack {
-                            Spacer().frame(maxWidth: 30.0)
-                            Text("My feelings").font(.system(size: 34.0, weight: .bold))
-                            Spacer()
-                        }
-                        
-                        
-                        
-                        HStack {
-                            Spacer().frame(maxWidth: 30.0)
-                            
-                            Text(item.writemore ?? "Nothing here!")
-                                .font(.system(size: 20.0))
-                                .multilineTextAlignment(.leading)
-                            
-                            Spacer()
-                        }
-                        
-                        HStack {
-                            Spacer().frame(maxWidth: 30.0)
-                            Text("Kind words for myself").font(.system(size: 34.0, weight: .bold))
-                            Spacer()
-                        }
-                        
-                        
-                        
-                        HStack {
-                            Spacer().frame(maxWidth: 30.0)
-                            
-                            Text(item.saynicetoyou ?? "Nothing here!")
-                                .font(.system(size: 20.0))
-                                .multilineTextAlignment(.leading)
-                            
-                            Spacer()
-                        }
-                    }
-                    .navigationTitle("Log Details")
-                    .navigationBarTitleDisplayMode(.inline)
+                ZStack {
+                    Color("background")
                     
-                }}
+                    VStack() {
+                        Spacer().frame(maxHeight: 150.0)
+                        
+                        Image(imgs[Int(item.emotion)])
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: 110.0)
+                        
+                        HStack {
+                            VStack(spacing: 7.0) {
+                                Text("details_studytime")
+                                    .font(.system(size: 20.0))
+                                    .foregroundColor(Color("TextColor"))
+                                
+                                Text("details_breaktime")
+                                    .font(.system(size: 20.0))
+                                    .foregroundColor(Color("TextColor"))
+                            }
+                            
+                            Spacer().frame(maxWidth: 10.0)
+                            
+                            VStack(spacing: 7.0) {
+                                Text("\(item.studyhours)h \(item.studyminutes)m")
+                                    .font(.system(size: 20.0))
+                                    .foregroundColor(Color("TextColor"))
+                                
+                                Text("\(item.breakhours)h \(item.breakminutes)m")
+                                    .foregroundColor(Color("TextColor"))
+                                    .font(.system(size: 20.0))
+                            }
+                        }
+                        
+                        ScrollView {
+                            VStack(spacing: 20.0) {
+                                Spacer().frame(maxWidth: 30.0)
+                                
+                                Spacer().frame(maxWidth: 30.0)
+                                
+                                HStack {
+                                    Spacer().frame(maxWidth: 30.0)
+                                    Text(item.subject ?? "No subject!")
+                                        .font(.system(size: 34.0, weight: .bold))
+                                        .foregroundColor(Color("TextColor"))
+                                    Spacer()
+                                }
+                                
+                                
+                                
+                                HStack {
+                                    Spacer().frame(maxWidth: 30.0)
+                                    
+                                    Text(item.whatdidyoustudy ?? "Nothing here!")
+                                        .font(.system(size: 20.0))
+                                        .foregroundColor(Color("TextColor"))
+                                        .multilineTextAlignment(.leading)
+                                    
+                                    Spacer()
+                                }
+                                
+                                if item.writemore != nil && item.writemore != "" {
+                                    
+                                    HStack {
+                                        Spacer().frame(maxWidth: 30.0)
+                                        Text("details_writemore")
+                                            .font(.system(size: 34.0, weight: .bold))
+                                            .foregroundColor(Color("TextColor"))
+                                        Spacer()
+                                    }
+                                    
+                                    
+                                    
+                                    HStack {
+                                        Spacer().frame(maxWidth: 30.0)
+                                        
+                                        Text(item.writemore ?? "Error retrieving data!")
+                                            .font(.system(size: 20.0))
+                                            .foregroundColor(Color("TextColor"))
+                                            .multilineTextAlignment(.leading)
+                                        
+                                        Spacer()
+                                    }
+                                }
+                                
+                                if item.saynicetoyou != nil && item.saynicetoyou != "" {
+                                    
+                                    HStack {
+                                        Spacer().frame(maxWidth: 30.0)
+                                        Text("details_nicewords")
+                                            .foregroundColor(Color("TextColor"))
+                                            .font(.system(size: 34.0, weight: .bold))
+                                        Spacer()
+                                    }
+                                    
+                                    
+                                    
+                                    HStack {
+                                        Spacer().frame(maxWidth: 30.0)
+                                        
+                                        Text(item.saynicetoyou ?? "Error retrieving data!")
+                                            .font(.system(size: 20.0))
+                                            .foregroundColor(Color("TextColor"))
+                                            .multilineTextAlignment(.leading)
+                                        
+                                        Spacer()
+                                    }
+                                }
+                            }
+                        }
+                        .navigationTitle("details_title")
+                        .navigationBarTitleDisplayMode(.inline)
+                    }
+                }.ignoresSafeArea()
+            }
         }
     }
 }
